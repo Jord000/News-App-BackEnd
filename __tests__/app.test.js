62832c7b6659a2d6545b11c458355fa57b86f71c
Jordan <jordanwatson000@gmail.com>
2023-11-21 10:29:16 +0000

    adds test data with more detail on properties

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,29 +1,29 @@
-const app = require('../MVC/app.js')
-const seed = require('../db/seeds/seed.js')
-const request = require('supertest')
-const db = require('../db/connection.js')
+const app = require('../MVC/app.js');
+const seed = require('../db/seeds/seed.js');
+const request = require('supertest');
+const db = require('../db/connection.js');
 const {
   userData,
   topicData,
   commentData,
   articleData,
-} = require('../db/data/test-data/index.js')
-const endpointFile = require('../endpoints.json')
-const jestSorted = require('jest-sorted')
+} = require('../db/data/test-data/index.js');
+const endpointFile = require('../endpoints.json');
+const jestSorted = require('jest-sorted');
 
 afterAll(() => {
-  db.end()
-})
+  db.end();
+});
 
 beforeEach(() => {
-  return seed({ topicData, userData, articleData, commentData })
-})
+  return seed({ topicData, userData, articleData, commentData });
+});
 
 describe('GET:200 /api/healthcheck', () => {
   test('makes correct 200 connection on a basic endpoint to check the health of the API', () => {
-    return request(app).get('/api/healthcheck').expect(200)
-  })
-})
+    return request(app).get('/api/healthcheck').expect(200);
+  });
+});
 
 describe('GET:200 /api/topics', () => {
   test('endpoint returns all topics as an array with slug and description', () => {
@@ -31,22 +31,22 @@ describe('GET:200 /api/topics', () => {
       .get('/api/topics')
       .expect(200)
       .then(({ body: { topics } }) => {
-        expect(topics.length).toEqual(topicData.length)
+        expect(topics.length).toEqual(topicData.length);
         topics.forEach((topic) => {
-          expect(topic).toHaveProperty('description')
-          expect(topic).toHaveProperty('slug')
-        })
-      })
-  })
+          expect(topic).toHaveProperty('description');
+          expect(topic).toHaveProperty('slug');
+        });
+      });
+  });
   test('provides error for incorrect api path spelling', () => {
     return request(app)
       .get('/api/topicsincorrect')
       .expect(404)
       .then((response) => {
-        expect(response.body.msg).toEqual('incorrect path - path not found')
-      })
-  })
-})
+        expect(response.body.msg).toEqual('incorrect path - path not found');
+      });
+  });
+});
 
 describe('GET:200 /api', () => {
   test('should respond with an object containing all endpoints', () => {
@@ -54,10 +54,10 @@ describe('GET:200 /api', () => {
       .get('/api')
       .expect(200)
       .then(({ body: { data } }) => {
-        expect(data).toMatchObject(endpointFile)
-      })
-  })
-})
+        expect(data).toMatchObject(endpointFile);
+      });
+  });
+});
 
 describe('GET:200 /api/articles', () => {
   test('should return all articles with the correct properties', () => {
@@ -70,19 +70,27 @@ describe('GET:200 /api/articles', () => {
       'votes',
       'article_img_url',
       'comment_count',
-    ]
+    ];
     return request(app)
       .get('/api/articles')
       .expect(200)
       .then(({ body: { articles } }) => {
-        expect(articles.length).toBe(articleData.length)
+        expect(articles.length).toBe(articleData.length);
         articles.forEach((article) => {
           expect(Object.keys(article)).toEqual(
             expect.arrayContaining(correctProperties)
-          )
-          expect(article).not.toHaveProperty('body')
-        })
-        expect(articles).toBeSortedBy('created_at', { descending: true })
-      })
-  })
-})
+          );
+          expect(article).toMatchObject({
+            author: expect.any(String),
+            title: expect.any(String),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+          });
+          expect(article).not.toHaveProperty('body');
+          expect(article.article_img_url).toMatch(new RegExp('^https:?'));
+        });
+        expect(articles).toBeSortedBy('created_at', { descending: true });
+      });
+  });
+});
